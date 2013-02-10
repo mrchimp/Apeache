972a78648f35b85bc460994ce8b4168c10d0d64c
mrchimp <chimpytk@gmail.com>
2013-02-10 03:28:20 +0000

    added favicon support

--- apeache.js
+++ apeache.js
@@ -1,27 +1,45 @@
 var http = require('http'),
     fs = require('fs'),
     url = require('url'),
-    qs = require('querystring');
+    qs = require('querystring'),
+    path = require('path'),
+    util = require('util');
 
 var Apeache = function () {};
 
 Apeache.visitor_count = 0;
 
 Apeache.start = function () {
-    this.server = http.createServer(this.respond).listen(1337, '127.0.0.1');
+    this.server = http.createServer(this.respond).listen(1337);
     console.log('Apeache Server running on port 1337');
 };
 
+Apeache.returnFile = function (res, filePath) {
+    var stat = fs.statSync(filePath);
+        
+    res.writeHead(200, {
+        'Content-Type': 'image/x-icon',
+        'Content-Length': stat.size
+    });
+    
+    var readStream = fs.createReadStream(filePath);
+    util.pump(readStream, res);
+};
+
 Apeache.respond = function (req, res) {
-    if (req.url === '/') {
+    if (req.url === '/favicon.ico') {
+        var filePath = path.join(__dirname, 'favicon.ico');
+        Apeache.returnFile(res, filePath)
+    } else if (req.url === '/') {
         req.on('end', function () {
             Apeache.visitor_count += 1;
             console.log('Visitor #' + Apeache.visitor_count + ': ' + req.connection.remoteAddress);
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write('<!DOCTYPE html><head><title>Apeache</title></head><body>');
-            res.write('Someone said: ' + Apeache.escapeHtml(fs.readFileSync('data.txt', 'utf-8')));
+            res.write('<p><strong>Someone said:</strong> ' + Apeache.escapeHtml(fs.readFileSync('data.txt', 'utf-8')) + '</p>');
             res.write(fs.readFileSync('form.html', 'utf-8'));
-            res.write(Apeache.visitor_count + ' visitors since last reboot.');
+            res.write('<p>' + Apeache.visitor_count + ' visitors since last reboot.</p>');
+            res.write('<p><a href="/">Refresh</a></p>');
             res.end('</body></html>');
         });
     } else if (req.url === '/post') {
@@ -42,12 +60,12 @@ Apeache.respond = function (req, res) {
             Apeache.visitor_count += 1;
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write('<!DOCTYPE html><head><title>Apeache</title></head><body>');
-
             fs.writeFile('data.txt', POST.usertext, function (err) {
                 if (err) { console.log(err); }
-                res.write('You said: ' + Apeache.escapeHtml(fs.readFileSync('data.txt', 'utf-8')));
+                res.write('<p><strong>You said:</strong> ' + Apeache.escapeHtml(fs.readFileSync('data.txt', 'utf-8')) + '</p>');
                 res.write(fs.readFileSync('form.html', 'utf-8'));
-                res.write(Apeache.visitor_count + ' visitors since last reboot.');
+                res.write('<p>' + Apeache.visitor_count + ' visitors since last reboot.</p>');
+                res.write('<p><a href="/">Refresh</a></p>');
                 res.end('</body></html>');
             });
         });
